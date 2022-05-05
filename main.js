3f7118694d838f8e19217177219cd118ab3d3293
Diego Duarte <diemanuel58@gmail.com>
2022-05-05 17:18:31 -0400

    Se logra generar Resultados.txt

--- main.js
+++ main.js
@@ -1,75 +1,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-
+const { ejecutarScrapGH } = require ('./scraping/githubscraper.js');
+//import {ejecutarScrapGH} from './scraping/githubscraper.js';
 //scrapGitHub("https://github.com/topics/c")
+//ejecutarScrapGH();
+process.setMaxListeners(Infinity);
 
-
-
-
-async function scrapGitHub(urlGH){
-    try{
-        const navegador = await puppeteer.launch(); //abrimos navegador
-        const pagina = await navegador.newPage(); //abrimos pagina
-        await pagina.goto(urlGH); //vamos al url y esperamos a que responda
-
-        descripcion = await pagina.evaluate(()=>{
-            return document.querySelector(".h3.color-fg-muted").textContent;
-        })
-
-        numero=(/(\d+(,\d*)*)/).exec(descripcion);
-        await navegador.close();
-        return numero[0];
-    } catch(error){
-        console.error();
-    }
-    
-}
-
-async function desaliasing (languages){
-    //var AliasJson = new XMLHttpRequest();
-    //console.log("xs");
-    //AliasJson.open('GET', './data/langAliases.json', false);
-    await fs.readFile('./data/langAliases.json', 'utf-8', (error,AliasJson) => {
-        console.log("ava33nce");
-        if (!error){
-            console.log("avance2");
-            aliasLan=JSON.parse(AliasJson);
-            //console.log(aliasLan.$('c++'));
-            for (var i=0;i<languages.length;i++){
-                actual=languages[i];
-                console
-                linkear="https://github.com/topics/" + aliasLan.$actual;
-                console.log(linkear);
-                totalEntry=scrapGitHub(linkear);
-                totalEntry=totalEntry.replace(',','');
-                console.log(languages[i]+":"+totalEntry);
-            }
-        }else{
-            console.log("avance");
-            console.log('Error de lectura: ${error}');
-        }
-    });
-    //AliasJson.
-    //AliasJson.send(null);
-    /*if(req.status == 200){
-        aliasLan=JSON.parse(AliasJson.responseText);
-        for (var i=0;i<languages.length;i++){
-            totalEntry=scrapGitHub("https://github.com/topics/"+aliasLan.languages[i]);
-            totalEntry=totalEntry.replace(',','');
-            console.log(languages[i]+":"+totalEntry);
-        }
-    }else{
-        
-        console.error("Error no se pudo leer json");
-    }*/
-    
-    console.log("avance"); 
-}
-
-
-//scrapGitHub("https://github.com/topics/c");
-
-
-let languages = ["python","c","java","c++","c#","visual basic","javascript","assembly language","sql","php","r","delphi/object pascal","go","swift","ruby","classic visual basic","objective-c","perl","lua","matlab"];
-desaliasing(languages);
-console.log("avance");
+ejecutarScrapGH();
--- scraping/githubscraper.js
+++ scraping/githubscraper.js
@@ -1,12 +1,9 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 //scrapGitHub("https://github.com/topics/c")
 
-
-
-
-async function scrapGitHub(urlGH){
+async function scrapGitHub(urlGH,language){
     try{
         const navegador = await puppeteer.launch(); //abrimos navegador
         const pagina = await navegador.newPage(); //abrimos pagina
@@ -18,36 +15,52 @@ async function scrapGitHub(urlGH){
 
         numero=(/(\d+(,\d*)*)/).exec(descripcion);
         await navegador.close();
-        return numero[0];
+        totalEntry=numero[0]
+        totalEntry=totalEntry.replace(',','');
+        totalEntry=language+"," +totalEntry+"\n";
+        console.log(language+", "+totalEntry);
+        fs.writeFileSync("./data/Resultados.txt", totalEntry, { flag:'a+' });
+        //console.log(numero[0]);
+        //return numero[0];
     } catch(error){
         console.error();
     }
     
 }
 
-function desaliasing (languages){
-    datps 
-    var AliasJson = new XMLHttpRequest();
-    AliasJson.open('GET', './data/langAliases.json', false);
-    AliasJson.send(null);
-    if(req.status == 200){
-        aliasLan=JSON.parse(AliasJson.responseText);
-        for (var i=0;i<languages.length;i++){
-            totalEntry=scrapGitHub("https://github.com/topics/"+aliasLan.languages[i]);
-            totalEntry=totalEntry.replace(',','');
-            console.log(languages[i]+":"+totalEntry);
+async function desaliasing (languages){
+    //var AliasJson = new XMLHttpRequest();
+    //console.log("xs");
+    //AliasJson.open('GET', './data/langAliases.json', false);
+    console.log("Antes");
+    try{
+        AliasJson=fs.readFileSync('./data/langAliases.json', 'utf-8');
+        AliasJson=JSON.parse(AliasJson);
+        console.log(AliasJson);
+        for (var i=0;i<20;i++){
+            actual=languages[i];
+            console
+            linkear="https://github.com/topics/" + AliasJson[languages[i]];
+            console.log(linkear);
+            await scrapGitHub(linkear,languages[i]);
         }
-    }else{
-        console.error("Error no se pudo leer json");
+    }catch(err){
+        console.error(err);
     }
-      
+    
 }
-
 
 //scrapGitHub("https://github.com/topics/c");
 
 async function ejecutarScrapGH(){
     let languages = ["python","c","java","c++","c#","visual basic","javascript","assembly language","sql","php","r","delphi/object pascal","go","swift","ruby","classic visual basic","objective-c","perl","lua","matlab",];
-    desaliasing(langauges);
+    desaliasing(languages);
 } 
 
+module.exports={
+    ejecutarScrapGH,
+};
+
+//export {ejecutarScrapGH};
+//export {desaliasing};
+//export {scrapGitHub};
