e08e9a08be4887dd4cffe5570a8a4cb0a48d4e7b
Manuel Rene Pauls Toews <rene.pauls@avantys.de>
2022-05-05 22:15:28 -0400

    auto open grafico

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const { ejecutarScrapGH } = require ('./scraping/githubscraper.js');
-//import {ejecutarScrapGH} from './scraping/githubscraper.js';
-//scrapGitHub("https://github.com/topics/c")
-//ejecutarScrapGH();
+const {ejecutarScrapGH} = require('./scraping/githubscraper.js');
+// import {ejecutarScrapGH} from './scraping/githubscraper.js';
+// scrapGitHub("https://github.com/topics/c")
+// ejecutarScrapGH();
 process.setMaxListeners(Infinity);
 
 ejecutarScrapGH();
+
+function getCommandLine() {
+  switch (process.platform) {
+    case 'darwin':
+      return 'open';
+    case 'win32':
+      return 'start';
+    case 'win64':
+      return 'start';
+    default:
+      return 'xdg-open';
+  }
+}
+
+require('child_process').exec(getCommandLine() + ' index.html');
--- scraping/datos.js
+++ scraping/datos.js
@@ -1,2 +1,3 @@
-var abcisa=["ruby","r","sql","matlab","lua","objective-c","assembly language","perl","delphi/object pascal","visual basic","classic visual basic"];
-var ordenada=["28232","26250","24174","9666","9607","5463","4742","3838","1897","232","232"];
+
+    var abcisa=["ruby","r","sql","matlab","lua","objective-c","assembly language","perl","delphi/object pascal","visual basic","classic visual basic"];
+    var ordenada=["28232","26250","24174","9666","9607","5463","4742","3838","1897","232","232"];
